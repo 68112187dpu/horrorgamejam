d7f9caa53dd0b48794326932f1e5e88fa2be0963
Belle065 <kanoachang.vcr@gmail.com>
2025-10-28 11:26:13 +0700

    Belle
    ยังไม่ได้แก้เพิ่ม

---
+++ Assets/Scenes/StartGame.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartGame : MonoBehaviour
+{
+    public void StartButton()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void ExitButton()
+    {
+        Debug.Log("Exit Game");
+        Application.Quit();
+    }
+}
