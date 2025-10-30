c69ef1fefbddd101f7adf8db04fdaaa2d37c2941
Belle065 <kanoachang.vcr@gmail.com>
2025-10-30 21:16:56 +0700

    NewScene

---
+++ Assets/Script/Character3.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Character3 : MonoBehaviour
+{
+    public void scenechanger(int sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex);
+    }
+
+
+
+
+
+
+}
